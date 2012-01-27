fe0e6922716322e99564a009e9fb002f2eb5765a
Warner Godfrey <warner@warnergodfrey.com>
2012-01-27 22:34:08 +1100

    Working on ubuntu, but now trying to work out why

--- lib/blinky/light.rb
+++ lib/blinky/light.rb
@@ -6,18 +6,7 @@ module Blinky
     
     def initialize device, recipe, plugins
         @handle = device.usb_open
-        begin
-          # ruby-usb bug: the arity of rusb_detach_kernel_driver_np isn't defined correctly, it should only accept a single argument.
-          if USB::DevHandle.instance_method(:usb_detach_kernel_driver_np).arity == 2
-            @handle.usb_detach_kernel_driver_np(INTERFACE_ID, INTERFACE_ID)
-          else
-            @handle.usb_detach_kernel_driver_np(INTERFACE_ID)
-          end
-        rescue Errno::ENODATA => e
-          # Already detached
-        end
         @handle.set_configuration(device.configurations.first)
-        @handle.claim_interface(INTERFACE_ID)
         self.extend(recipe)   
         plugins.each do |plugin|
             self.extend(plugin)
